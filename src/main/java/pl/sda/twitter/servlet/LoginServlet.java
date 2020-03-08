8b580528ebee4b0010ca6ba496dab6bc0b7dda7e
Ela <elzbieta.maksym@gmail.com>
2020-03-08 12:46:03 +0100

    dodanie obsugi logowania

--- src/main/java/pl/sda/twitter/servlet/LoginServlet.java
+++ src/main/java/pl/sda/twitter/servlet/LoginServlet.java
@@ -1,34 +1,46 @@
 package pl.sda.twitter.servlet;
 
+import pl.sda.twitter.exceptions.IncorrectLoginOrPasswordException;
+import pl.sda.twitter.persistance.entities.TbUser;
+import services.UserService;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+    private UserService userService = new UserService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         final String login = req.getParameter("login");
         final String password = req.getParameter("password");
 
-        final PrintWriter writer = res.getWriter();
-        final String userName = "admin";
-
-        if(login.equals(userName) && password.equals("password")){
-            final HttpSession session = req.getSession();
-
-            session.setAttribute("user", userName);
-            res.sendRedirect(req.getContextPath() + "/");
-
-
-        } else {
-            writer.write("Niepoprawne dane logowania");
+        try {
+            final TbUser user = userService.getUser(login, password);
+            resp.sendRedirect("index.jsp");
+        } catch (IncorrectLoginOrPasswordException e){
+            resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            resp.sendRedirect("login.jsp");
         }
+
+//        final PrintWriter writer = res.getWriter();
+//        final String userName = "admin";
+//
+//        if(login.equals(userName) && password.equals("password")){
+//            final HttpSession session = req.getSession();
+//
+//            session.setAttribute("user", userName);
+//            res.sendRedirect(req.getContextPath() + "/");
+//
+//
+//        } else {
+//            writer.write("Niepoprawne dane logowania");
+//        }
     }
 
 }
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,25 @@
+package services;
+
+import pl.sda.twitter.exceptions.IncorrectLoginOrPasswordException;
+import pl.sda.twitter.persistance.dao.UserDao;
+import pl.sda.twitter.persistance.entities.TbUser;
+
+public class UserService {
+
+    private UserDao userDao = new UserDao();
+
+    public TbUser getUser (String username, String password)
+            throws IncorrectLoginOrPasswordException {
+
+
+        final TbUser user = userDao.getUserByLogin(username);
+
+        if (user==null || !user.getPassword().equals(password)){
+            throw new IncorrectLoginOrPasswordException("password or username does not match");
+        }
+
+
+        return user;
+
+    }
+}
